30eb0679c0c1ac52d50e32ecf8b72651ca9f61de
Architsharma7 <architsharma7911@gmail.com>
2022-12-19 23:27:37 +0530

    added

--- mentalwellbeingapp/components/footer.js
+++ mentalwellbeingapp/components/footer.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 const footer = () => {
   return (
-    <div>footer</div>
+    <div className="bg-black">
+    <div className="bg-gray-800">
+      <footer className="p-4 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6">
+        <span className="text-sm text-gray-500 sm:text-center">
+          Made By Archit @Arcsh7
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 sm:mt-0">
+          <li>
+            <a href="/" className="hover:underline">
+              Github
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </div>
+    </div>
   )
 }
 
