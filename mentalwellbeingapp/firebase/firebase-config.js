cc5f18810904032f623e146414093de5e0f35ceb
Architsharma7 <architsharma7911@gmail.com>
2022-12-29 14:24:39 +0530

    updated signin

---
+++ mentalwellbeingapp/firebase/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDxQfzMsANC6iNxnvsrcIUJfDAcpOlZYXQ",
+  authDomain: "mentalwellbeing-7.firebaseapp.com",
+  projectId: "mentalwellbeing-7",
+  storageBucket: "mentalwellbeing-7.appspot.com",
+  messagingSenderId: "1021121220178",
+  appId: "1:1021121220178:web:9afcc6a2bde40f3e06198b"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
