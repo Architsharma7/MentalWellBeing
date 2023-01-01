d54d12d53caed53b9528a039cd2568da6686284e
Architsharma7 <architsharma7911@gmail.com>
2023-01-01 18:19:58 +0530

    responsiveness completed

--- mentalwellbeingapp/components/homepage.js
+++ mentalwellbeingapp/components/homepage.js
@@ -12,12 +12,12 @@ const Homepage = () => {
     {
       img: "",
       title: "hello",
-      desc: "",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel temporibus impedit minima dolores nihil, blanditiis asperiores debitis ad, ratione repudiandae recusandae doloremque nesciunt vitae atque iure. Hic debitis cupiditate atque?",
     },
     {
       img: "",
       title: "hello",
-      desc: "",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores dolore nisi amet quam! Veniam eum omnis animi placeat, illo fuga neque quaerat nobis molestias. Nam voluptate dolorem unde? Sequi, eum.",
     },
   ];
 
@@ -55,16 +55,16 @@ const Homepage = () => {
     <div className="bg-violet-200 flex flex-col">
       <div className="mb-20">
         {/* heading for the project short description */}
-        <div className="w-screen h-screen bg-indigo-500 flex items-center">
-          <div className="w-1/2 float-left justify-start mx-10">
-            <p className="text-white text-5xl">
+        <div className="w-screen md:h-screen lg:h-screen xl:h-screen bg-indigo-500 flex items-center h-screen">
+          <div className="md:w-1/2 lg:w-1/2 xl:w-1/2 2xl:w-1/2 md:float-left lg:float-left xl:float-left md:justify-start lg:justify-start xl:justify-start justify-center mx-10 mt-10 md:mt-0 lg:mt-0 xl:mt-0 mb-20 md:mb-0 lg:mb-0 xl:mb-0">
+            <p className="text-white text-4xl md:text-5xl lg:text-5xl xl:text-5xl text-center md:text-left lg:text-left xl:text-left">
               hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
               odit voluptatum, eveniet minus sed dolorem corrupti deleniti unde
               enim, repellendus architecto quaerat hic reiciendis, quis nihil
               molestiae! Similique, incidunt consectetur?
             </p>
           </div>
-          <div className="w-1/2 float-right justify-end mx-10">
+          <div className="md:w-1/2 lg:w-/2 xl:w-1/2 md:float-right lg:float-right xl:float-right md:justify-end lg:justify-end xl:justify-end md:mx-10 lg:mx-10 xl:mx-10 hidden">
             {/* svg goes here */}
           </div>
           {/* <div className='absolute w-full h-56 bottom-0' id='curve'></div> */}
@@ -74,7 +74,7 @@ const Homepage = () => {
         <div>
           <p className="text-sm flex justify-center mt-10">About</p>
           <div className=" mt-20">
-            <div className="grid gap-x-6 gap-y-5 w-4/5 mx-auto grid-cols-3">
+            <div className="grid gap-x-6 gap-y-10 w-4/5 mx-auto grid-rows-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3">
               {cards.map((items, key) => {
                 return (
                   <div
@@ -103,7 +103,7 @@ const Homepage = () => {
       </div>
       <div className="bg-white w-screen">
         <div className="mt-20">
-          <div className="flex justify-start mx-10 w-1/2 float-left border-2 border-indigo-600 p-4 rounded-lg">
+          <div className="flex md:justify-start lg:justify-start xl:justify-start justify-center mx-10 md:w-1/2 lg:w-1/2 xl:w-1/2 md:float-left lg:float-left xl:float-left border-2 border-indigo-600 p-4 rounded-lg">
             <img src="bgsquare.jpg" alt="" className="h-20 mr-4 rounded-full" />
             <p className="justify-center">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo magni
@@ -112,7 +112,7 @@ const Homepage = () => {
               sint tempore hic cum voluptatem?
             </p>
           </div>
-          <div className="flex justify-end mx-10 w-1/2 float-right mt-20 border-2 border-indigo-600 p-4 rounded-lg mb-20">
+          <div className="flex md:justify-end lg:justify-end xl:justify-end justify-center mx-10 md:w-1/2 lg:w-1/2 xl:w-1/2 md:float-right lg:float-right xl:float-right border-2 border-indigo-600 p-4 rounded-lg mb-20 mt-20">
             <img src="bgsquare.jpg" alt="" className="h-20 mr-4 rounded-full" />
             <p className="justify-center">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo magni
@@ -123,14 +123,14 @@ const Homepage = () => {
           </div>
         </div>
       </div>
-      <div className="bg-indigo-500 w-screen mb-20">
+      <div className="bg-indigo-500 w-screen">
         <div className="mt-20 mx-20">
-          <div className="flex flex-row justify-start w-2/3 float-left">
+          <div className="md:flex lg:flex xl:flex flex-row md:justify-start lg:justify-start xl:justify-start md:w-2/3 lg:w-2/3 xl:w-2/3 md:float-left lg:float-left xl:float-left hidden md:mb-24 lg:mb-24 xl:mb-24">
             <img src="worldmap2.png" alt="" className="w-4/5" />
           </div>
-          <div className="flex justify-end w-1/3 float-right flex-row align-middle">
+          <div className="flex md:justify-end lg:justify-end xl:justify-end md:w-1/3 lg:w-1/3 xl:w-1/3 md:float-right lg:float-right xl:float-right justify-center flex-row align-middle mb-20">
             <div className="flex flex-col">
-              <p className="text-white justify-center float-left">
+              <p className="text-white justify-center md:float-left lg:float-left xl:float-left text-center">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Dignissimos aperiam aliquam sint nostrum itaque, cumque dolores
                 natus, dolore deserunt repudiandae neque quas. Esse repellendus
@@ -142,12 +142,14 @@ const Homepage = () => {
                 value={userInput}
                 onChange={onUserChangedText}
               />
-                <button
-                  className="mt-4 bg-blue-500 w-1/4 justify-center px-6 py-2 rounded-2xl text-center text-white"
-                  onClick={callGenerateEndpoint}
-                >
-                {isGenerating ? "loading..." : "search"}
-                </button>
+              <div className="flex justify-center">
+              <button
+                className="mt-4 bg-blue-500 flex justify-center px-12 py-2 rounded-2xl text-white w-20"
+                onClick={callGenerateEndpoint}
+              >
+                {isGenerating ? "Loading..." : "Search"}
+              </button>
+              </div>
               {apiOutput && (
                 <div className="mt-6 text-white justify-center">
                   <p className="text-md">{apiOutput}</p>
--- mentalwellbeingapp/components/navbar.js
+++ mentalwellbeingapp/components/navbar.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { Transition } from "@headlessui/react";
 
 const Navbar = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className=" bg-violet-200 px-2 sm:px-4 py-2.5 w-full z-20 top-0 left-0 border-b border-gray-200">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
@@ -11,17 +15,11 @@ const Navbar = () => {
         </a>
         <div className="flex md:order-2">
           <button
-            data-collapse-toggle="navbar-sticky"
-            type="button"
             className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-            aria-controls="navbar-sticky"
-            aria-expanded="false"
+            onClick={() => setIsOpen(!isOpen)}
           >
-            <span className="sr-only">Open main menu</span>
             <svg
               className="w-6 h-6"
-              aria-hidden="true"
-              fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
@@ -32,16 +30,16 @@ const Navbar = () => {
               ></path>
             </svg>
           </button>
-        </div>
+        </div> 
         <div
           className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
           id="navbar-sticky"
         >
-          <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-violet-200 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-violet-200">
+          <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg lg:bg-violet-200 xl:bg-violet-200 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-violet-200 bg-white">
             <li>
               <a
                 href="#"
-                className="block py-2 pl-3 pr-4 text-black rounded md:hover:text-blue-700 md:p-0"
+                className="block py-2 pl-3 pr-4 text-black rounded md:hover:text-blue-700 md:p-0 text-center border-b md:border-b-0"
                 aria-current="page"
               >
                 Blog
@@ -50,7 +48,7 @@ const Navbar = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 pl-3 pr-4 text-black rounded md:hover:text-blue-700 md:p-0"
+                className="block py-2 pl-3 pr-4 text-black rounded md:hover:text-blue-700 md:p-0 text-center border-b md:border-b-0"
               >
                 Chat
               </a>
@@ -58,7 +56,7 @@ const Navbar = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 pl-3 pr-4 text-black rounded md:hover:text-blue-700 md:p-0"
+                className="block py-2 pl-3 pr-4 text-black rounded md:hover:text-blue-700 md:p-0 text-center"
               >
                 Quiz
               </a>
