f65262699aec65baad31ed820aa39abd85b0e1bb
Architsharma7 <architsharma7911@gmail.com>
2023-04-26 19:58:56 +0530

    origin updated to backend

--- chat-server/index.js
+++ chat-server/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "ment-well-backend-v2.vercel.app",
     methods: ["GET", "POST"],
   },
 });
