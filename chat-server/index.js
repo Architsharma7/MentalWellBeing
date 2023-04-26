d311b971f0818ee8fdd6876ff0a57ff6b5d0540a
Architsharma7 <architsharma7911@gmail.com>
2023-04-26 20:30:09 +0530

    origin changed

--- chat-server/index.js
+++ chat-server/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "mentwell.vercel.app/chats",
+    origin: "mentwell.vercel.app/chat",
     methods: ["GET", "POST"],
   },
 });
